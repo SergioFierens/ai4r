6c9861380465efa036ee3dc4b33ed8c7bd61547d
Sergio Fierens <sergio.fierens@gmail.com>
2009-04-01 09:56:41 +0100

    Rolling back changes on single linkage

--- lib/ai4r/clusterers/single_linkage.rb
+++ lib/ai4r/clusterers/single_linkage.rb
@@ -21,21 +21,16 @@ module Ai4r
     # distance between the two closest elements in the two clusters.
     class SingleLinkage < Clusterer
       
-      attr_reader :data_set, :number_of_clusters, :clusters, :dendrogram
+      attr_reader :data_set, :number_of_clusters, :clusters
       
       parameters_info :distance_function => 
           "Custom implementation of distance function. " +
           "It must be a closure receiving two data items and return the " +
           "distance bewteen them. By default, this algorithm uses " + 
-          "squared ecuclidean distance of numeric attributes",
-          :complete_dendrogram => "If false, the clusterer will halt the "+
-          "building when it reaches the required number of clusters. If true, "+
-          "it will continue to complete the dendrogram with a unique root. " +
-          "False by default"
+          "ecuclidean distance of numeric attributes to the power of 2."
       
       def initialize
-        @distance_function = lambda {|a,b| euclidean_distance(a, b)}
-        @complete_dendrogram = false
+        @distance_function = nil
       end
       
       # Build a new clusterer, using data examples found in data_set.
@@ -45,12 +40,13 @@ module Ai4r
         @data_set = data_set
         @number_of_clusters = number_of_clusters
         
-        create_initial_dendrogram
+        index_clusters = create_initial_index_clusters
         create_distance_matrix(data_set)
-        while not complete_condition_met
-          merge_clusters(get_closest_clusters)
+        while index_clusters.length > @number_of_clusters
+          clusters_to_merge = get_closest_clusters(index_clusters)
+          index_clusters = merge_clusters(clusters_to_merge, index_clusters)
         end
-        @clusters = build_clusters_from_dendrogram
+        @clusters = build_clusters_from_index_clusters index_clusters
         
         return self
       end
@@ -58,32 +54,31 @@ module Ai4r
       # Classifies the given data item, returning the cluster index it belongs 
       # to (0-based).
       def eval(data_item)
-        get_min_index(@clusters.collect do |cluster| 
-            distance_between_item_and_cluster(data_item, cluster)
-        end)
+        get_min_index(@clusters.collect {|cluster| 
+            distance_between_item_and_cluster(data_item, cluster)})
       end
       
-      ###########################
+      # This function calculates the distance between 2 different
+      # instances. By default, it returns the euclidean distance to the 
+      # power of 2.
+      # You can provide a more convinient distance implementation:
+      # 
+      # 1- Overwriting this method
+      # 
+      # 2- Providing a closure to the :distance_function parameter
+      def distance(a, b)
+        return @distance_function.call(a, b) if @distance_function
+        return euclidean_distance(a, b)
+      end
+      
       protected
       
-      # if complete_dendrogram is true, complete condition is met
-      # when there is only one cluster in the dendrogram.
-      # If complete_dendrogram is false, complete condition is met
-      # when there are as many clusters in the dendrogram as required by user.
-      def complete_condition_met
-        (@dendrogram.last.length <= @number_of_clusters && 
-            !@complete_dendrogram) ||
-        (@dendrogram.last.length == 1 && 
-            @complete_dendrogram)  
-      end
-      
-      # Builds the first level of the dendrogram, with a
-      # singleton cluster for each data item.
-      # [[ [0], [1], [2], ... , [n-1] ]
-      def create_initial_dendrogram
+      # returns [ [0], [1], [2], ... , [n-1] ]
+      # where n is the number of data items in the data set
+      def create_initial_index_clusters
         index_clusters = []
         @data_set.data_items.length.times {|i| index_clusters << [i]}
-        @dendrogram = [index_clusters]
+        return index_clusters
       end
       
       # Create a partial distance matrix:
@@ -100,7 +95,7 @@ module Ai4r
         data_set.data_items.each_with_index do |a, i|
           i.times do |j|
             b = data_set.data_items[j]
-            @distance_matrix[i-1][j] = @distance_function.call(a, b)
+            @distance_matrix[i-1][j] = distance(a, b)
           end
         end
       end
@@ -113,15 +108,42 @@ module Ai4r
         return @distance_matrix[index_a-1][index_b]
       end
 
-      # Return an array with the indexes in the previous dendrogram level of 
-      # the two closest clusters => [index_cluster_a, index_cluster_b]
-      def get_closest_clusters
+      # clusters_to_merge = [index_cluster_a, index_cluster_b].
+      # cluster_a and cluster_b are removed from index_cluster, 
+      # and a new cluster with all members of cluster_a and cluster_b
+      # is added. 
+      # It returns the new clusters array.
+      def merge_clusters(clusters_to_merge, index_clusters)
+        index_a = clusters_to_merge.first
+        index_b = clusters_to_merge.last
+        index_a, index_b = index_b, index_a if index_b > index_a
+        new_index_cluster = index_clusters[index_a] +
+          index_clusters[index_b]
+        index_clusters.delete_at index_a
+        index_clusters.delete_at index_b
+        index_clusters << new_index_cluster
+        return index_clusters
+      end
+   
+      # Given an array with clusters of data_items indexes, 
+      # it returns an array of data_items clusters 
+      def build_clusters_from_index_clusters(index_clusters)
+        @distance_matrix = nil
+        return index_clusters.collect do |index_cluster|
+          Ai4r::Data::DataSet.new(:data_labels => @data_set.data_labels,
+            :data_items => index_cluster.collect {|i| @data_set.data_items[i]})
+        end
+      end
+      
+      # Returns ans array with the indexes of the two closest
+      # clusters => [index_cluster_a, index_cluster_b]
+      def get_closest_clusters(index_clusters)
         min_distance = 1.0/0
         closest_clusters = [1, 0]
-        @dendrogram.last.each_with_index do |cluster_a, index_a|
+        index_clusters.each_with_index do |cluster_a, index_a|
           index_a.times do |index_b|
-            cluster_b = @dendrogram.last[index_b]
-            cluster_distance = calc_clusters_distance(cluster_a, cluster_b)
+            cluster_b = index_clusters[index_b]
+            cluster_distance = calc_index_clusters_distance(cluster_a, cluster_b)
             if cluster_distance < min_distance
               closest_clusters = [index_a, index_b]
               min_distance = cluster_distance
@@ -131,107 +153,27 @@ module Ai4r
         return closest_clusters
       end
       
-      # Calculate distance between clusters, browsing the dendrogram 
-      # recursively. 
-      # In each level of the dendrogram we have arrays with  1 item 
-      # (i.e. index of the same cluster in the previous level) or 
-      # with 2 items (indexes of the 2 merged clusters in the previous level).
-      # The distance between 2 clusters with 1 item is calculated with 
-      # the distance matrix, and the distance between a cluster with
-      # one item and a cluster with the merge of two previous clusters is 
-      # calculated with the linkage function.
-      def calc_clusters_distance(cluster_a, cluster_b, level_a=nil, level_b=nil)
-        level_a ||= @dendrogram.length-1
-        level_b ||= @dendrogram.length-1
-      
-        if level_a==0 && level_b==0
-          return read_distance_matrix(cluster_a.first, cluster_b.first)
+      # Calculate cluster distance using the single linkage method
+      def calc_index_clusters_distance(cluster_a, cluster_b)
+        min_dist = 1.0/0
+        cluster_a.each do |index_a|
+          cluster_b.each do |index_b|
+            dist = read_distance_matrix(index_a, index_b)
+            min_dist = dist if dist < min_dist
+          end
         end
-
-        if cluster_a.length > 1
-          return linkage(
-              calc_clusters_distance(@dendrogram[level_a-1][cluster_a.first], 
-                cluster_b, level_a-1, level_b),
-              calc_clusters_distance(@dendrogram[level_a-1][cluster_a.last], 
-                cluster_b, level_a-1, level_b))
-        elsif cluster_b.length > 1
-          return linkage(  
-              calc_clusters_distance(cluster_a, 
-                @dendrogram[level_b-1][cluster_b.first], level_a, level_b-1),
-              calc_clusters_distance(cluster_a, 
-                @dendrogram[level_b-1][cluster_b.last], 
-                level_a, level_b-1))
-        elsif level_a > 0
-          return calc_clusters_distance(@dendrogram[level_a-1][cluster_a.first], 
-            cluster_b, level_a-1, level_b)
-        elsif level_b > 0
-          return calc_clusters_distance(cluster_a, 
-              @dendrogram[level_b-1][cluster_b.first],
-                level_a, level_b-1)
-        end
+        return min_dist
       end
       
-      # Use the single linkage method (Nearest-neighbor)
-      def linkage(a, b)
-        [a, b].min
-      end
-      
-      # Adds a new level in the dendrogram, using the indexes
-      # of clusters in the previous level.
-      # clusters_to_merge = [index_cluster_a, index_cluster_b].
-      def merge_clusters(clusters_to_merge)
-        new_level = []
-        @dendrogram.last.length.times do |i| 
-          new_level << [i] if !clusters_to_merge.include? i
-        end
-        new_level << clusters_to_merge
-        @dendrogram << new_level
-      end
-
       def distance_between_item_and_cluster(data_item, cluster)
         min_dist = 1.0/0
         cluster.data_items.each do |another_item|
-          dist = @distance_function.call(data_item, another_item)
+          dist = distance(data_item, another_item)
           min_dist = dist if dist < min_dist
         end
         return min_dist
       end
       
-      # it returns an array of data sets, representing each clusters 
-      def build_clusters_from_dendrogram
-        @dendrogram.each {|l| puts l.inspect}
-        k_level = dendrogram_level_with_k_clusters(@number_of_clusters)
-        puts "LEVEL: #{k_level}"
-        cluster_items = @dendrogram[k_level].collect do |cluster_data|
-          items = []
-          collect_items(items, cluster_data, k_level)
-          items
-        end
-        cluster_items.each {|l| puts l.inspect}
-        return cluster_items.collect do |data_items|
-          Ai4r::Data::DataSet.new(:data_labels => @data_set.data_labels,
-            :data_items => data_items)
-        end
-      end
-      
-      # Return the dendrogram level that contains k clusters
-      def dendrogram_level_with_k_clusters(k)
-        @data_set.data_items.length-k
-      end
-      
-      # return an array of data items, using the specified dendrogram level
-      # [ item 0, item 1, ... item n]
-      def collect_items(collection, indexes, level)
-        if level == 0
-          collection << @data_set.data_items[indexes.first] 
-        else
-          indexes.each do |i| 
-            collect_items(collection, @dendrogram[level-1][i], level-1)
-          end  
-        end
-      end
-      
     end
   end
 end
-
--- test/clusterers/single_linkage_test.rb
+++ test/clusterers/single_linkage_test.rb
@@ -1,61 +1,49 @@
 require 'test/unit'
 require File.dirname(__FILE__) + '/../../lib/ai4r/clusterers/single_linkage'
-
+ 
 class Ai4r::Clusterers::SingleLinkage
-  attr_accessor :data_set, :number_of_clusters, :clusters, :distance_matrix, :dendrogram
+  attr_accessor :data_set, :number_of_clusters, :clusters, :distance_matrix
 end
-
+ 
 class Ai4r::Clusterers::SingleLinkageTest < Test::Unit::TestCase
   
   include Ai4r::Clusterers
   include Ai4r::Data
-
-  @@data = [  [10, 3], [3, 10], [2, 8], [2, 5], [3, 8], [10, 3],
+ 
+  @@data = [ [10, 3], [3, 10], [2, 8], [2, 5], [3, 8], [10, 3],
               [1, 3], [8, 1], [2, 9], [2, 5], [3, 3], [9, 4]]
             
-  @@expected_distance_matrix = [ 
-        [98.0], 
-        [89.0, 5.0], 
-        [68.0, 26.0, 9.0], 
-        [74.0, 4.0, 1.0, 10.0], 
-        [0.0, 98.0, 89.0, 68.0, 74.0], 
-        [81.0, 53.0, 26.0, 5.0, 29.0, 81.0], 
-        [8.0, 106.0, 85.0, 52.0, 74.0, 8.0, 53.0], 
-        [100.0, 2.0, 1.0, 16.0, 2.0, 100.0, 37.0, 100.0], 
-        [68.0, 26.0, 9.0, 0.0, 10.0, 68.0, 5.0, 52.0, 16.0], 
-        [49.0, 49.0, 26.0, 5.0, 25.0, 49.0, 4.0, 29.0, 37.0, 5.0], 
+  @@expected_distance_matrix = [
+        [98.0],
+        [89.0, 5.0],
+        [68.0, 26.0, 9.0],
+        [74.0, 4.0, 1.0, 10.0],
+        [0.0, 98.0, 89.0, 68.0, 74.0],
+        [81.0, 53.0, 26.0, 5.0, 29.0, 81.0],
+        [8.0, 106.0, 85.0, 52.0, 74.0, 8.0, 53.0],
+        [100.0, 2.0, 1.0, 16.0, 2.0, 100.0, 37.0, 100.0],
+        [68.0, 26.0, 9.0, 0.0, 10.0, 68.0, 5.0, 52.0, 16.0],
+        [49.0, 49.0, 26.0, 5.0, 25.0, 49.0, 4.0, 29.0, 37.0, 5.0],
         [2.0, 72.0, 65.0, 50.0, 52.0, 2.0, 65.0, 10.0, 74.0, 50.0, 37.0]]
   
   def setup
     SingleLinkage.send(:public, *SingleLinkage.protected_instance_methods)
   end
-
-  def test_complete_condition_met
+  
+  def test_build
     clusterer = Ai4r::Clusterers::SingleLinkage.new
-    clusterer.dendrogram = [[1,2,3,4]]
-    
-    clusterer.complete_dendrogram = true
-    clusterer.number_of_clusters = 4
-    assert !clusterer.complete_condition_met
-    
-    clusterer.complete_dendrogram = false
-    clusterer.number_of_clusters = 4
-    assert clusterer.complete_condition_met
-    
-    clusterer.complete_dendrogram = false
-    clusterer.number_of_clusters = 3
-    assert !clusterer.complete_condition_met
+    clusterer.build(DataSet.new(:data_items => @@data), 4)
+    #draw_map(clusterer)
+    assert_equal 4, clusterer.clusters.length
   end
   
-  def test_create_initial_dendrogram
+  def test_eval
     clusterer = Ai4r::Clusterers::SingleLinkage.new
-    clusterer.data_set = DataSet.new(:data_items => @@data)
-    clusterer.create_initial_dendrogram
-    assert_equal @@data.length, clusterer.dendrogram.first.length
-    assert_equal [0], clusterer.dendrogram.first.first
-    assert_equal [@@data.length-1], clusterer.dendrogram.first.last
+    clusterer.build(DataSet.new(:data_items => @@data), 4)
+    assert_equal 2, clusterer.eval([0,8])
+    assert_equal 0, clusterer.eval([8,0])
   end
-  
+ 
   def test_create_distance_matrix
     clusterer = Ai4r::Clusterers::SingleLinkage.new
     clusterer.create_distance_matrix(DataSet.new(:data_items => @@data))
@@ -73,61 +61,22 @@ class Ai4r::Clusterers::SingleLinkageTest < Test::Unit::TestCase
     assert_equal 9.0, clusterer.read_distance_matrix(2, 3)
     assert_equal 0, clusterer.read_distance_matrix(5, 5)
   end
-
-  def test_get_closest_clusters
-    data_set = DataSet.new(:data_items => @@data[0..4])
-    clusterer = Ai4r::Clusterers::SingleLinkage.new
-    clusterer.data_set = data_set
-    clusterer.create_initial_dendrogram
-    clusterer.create_distance_matrix(data_set)
-    assert_equal [4, 2], clusterer.get_closest_clusters
-  end
   
-  def test_calc_clusters_distance
-    data_set = DataSet.new(:data_items => @@data[0..4])
-    clusterer = Ai4r::Clusterers::SingleLinkage.new
-    clusterer.data_set = data_set
-    clusterer.create_distance_matrix(data_set)
-    clusterer.dendrogram = [[[0],[1],[2],[3]], [[1], [2], [0,3]], [[0,1],[2]]]
-    assert_equal 9, clusterer.calc_clusters_distance([0,1], [2])
-    assert_equal 98, clusterer.calc_clusters_distance([0], [1], 0, 0)
-  end
-  
-  def test_linkage
-    clusterer = Ai4r::Clusterers::SingleLinkage.new
-    assert_equal 3.99, clusterer.linkage(3.99, 4)
-    assert_equal 3.99, clusterer.linkage(4, 3.99)
-  end
-  
-  def ntest_build
-    clusterer = Ai4r::Clusterers::SingleLinkage.new
-    clusterer.build(DataSet.new(:data_items => @@data), 4)
-    #draw_map(clusterer)
-    assert_equal 4, clusterer.clusters.length
-  end
-  
-  def notest_eval
-    clusterer = Ai4r::Clusterers::SingleLinkage.new
-    clusterer.build(DataSet.new(:data_items => @@data), 4)
-    assert_equal 2, clusterer.eval([0,8])
-    assert_equal 0, clusterer.eval([8,0])
-  end
-
-  def ntest_calc_index_clusters_distance
+  def test_calc_index_clusters_distance
     clusterer = Ai4r::Clusterers::SingleLinkage.new
     clusterer.distance_matrix = @@expected_distance_matrix
     assert_equal 98.0, clusterer.calc_index_clusters_distance([0], [1])
     assert_equal 4.0, clusterer.calc_index_clusters_distance([0, 1], [3, 4])
   end
   
-  def ntest_get_closest_clusters
+  def test_get_closest_clusters
     clusterer = Ai4r::Clusterers::SingleLinkage.new
     clusterer.distance_matrix = @@expected_distance_matrix
     assert_equal [1,0], clusterer.get_closest_clusters([[0,1], [3,4]])
     assert_equal [2,1], clusterer.get_closest_clusters([[3,4], [0,1], [5,6]])
    end
    
-  def ntest_create_initial_index_clusters
+  def test_create_initial_index_clusters
     clusterer = Ai4r::Clusterers::SingleLinkage.new
     clusterer.data_set = DataSet.new :data_items => @@data
     index_clusters = clusterer.create_initial_index_clusters
@@ -136,7 +85,7 @@ class Ai4r::Clusterers::SingleLinkageTest < Test::Unit::TestCase
     assert_equal @@data.length-1, index_clusters.last.first
   end
   
-  def ntest_merge_clusters
+  def test_merge_clusters
     clusterer = Ai4r::Clusterers::SingleLinkage.new
     clusters = clusterer.merge_clusters([1,2], [[1,2],[3,4],[5,6]])
     assert_equal [[1,2], [3,4,5,6]], clusters.collect {|x| x.sort}
@@ -144,9 +93,9 @@ class Ai4r::Clusterers::SingleLinkageTest < Test::Unit::TestCase
     assert_equal [[1,2], [3,4,5,6]], clusters.collect {|x| x.sort}
   end
   
-  def ntest_distance_between_item_and_cluster
+  def test_distance_between_item_and_cluster
     clusterer = Ai4r::Clusterers::SingleLinkage.new
-    assert_equal 8.0, clusterer.distance_between_item_and_cluster([1,2], 
+    assert_equal 8.0, clusterer.distance_between_item_and_cluster([1,2],
       DataSet.new(:data_items => [[3,4],[5,6]]))
   end
   
@@ -162,4 +111,3 @@ class Ai4r::Clusterers::SingleLinkageTest < Test::Unit::TestCase
   end
   
 end
-
