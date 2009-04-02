76376e127af6f7c5845f15ac997f33068abb14e0
Sergio Fierens <sergio.fierens@gmail.com>
2009-04-02 17:57:00 +0100

    Work on clusterer example continues

--- examples/clusterers/google_search.rb
+++ examples/clusterers/google_search.rb
@@ -2,30 +2,26 @@ require 'rubygems'
 require 'hpricot'
 require 'net/http'
 
-WORDS_TO_IGNORE = %w(a all am an and any are as at be but can did do does for
-  from had has have here how i if in is it no not of on or so that the then
-  there this to too up use what when where who why you by | - was with s ) << ""
+def google_search(query, results)
+  search_results = []
+  start = 0
+  while search_results.length<results
+    puts "searching #{query} #{start}" 
+    search_results += get_10_google_results(query, start)
+    start+=10
+  end
+  return search_results
+end
 
-def search_google(query, start)
-  query.gsub!(/\s/, "%20")
+def get_10_google_results(query, start)
+  query = query.gsub(/\s/, "%20")
   query_url = "/search?q=#{query}&btnG=Google+Search&start=#{start}"
   response = Net::HTTP.get_response('www.google.com', query_url)
   doc = Hpricot(response.body)
   doc.search("span[@class='gl']").remove  #Remove "Cached" "Similiar cites" links
   doc.search("cite").remove #Remove site url
   search_results = doc.search("//li[@class='g']") #Get individual results
-  results = []
-  search_results.each do |search_result|
-    title = search_result.search("h3 a").text
-    description = search_result.search("div[@class='s']").text
-    results << {:title => title, :description => description}
+  return search_results.collect do |search_result|
+    search_result.search("div[@class='s']").text
   end
-  return results
-end
-
-def parse_keywords(text, query)
-  text.
-    split(/[\s,;:\.\(\)\[\]\?\']/).     #Split words
-    collect {|token| token.downcase}.  #Everything in lower case
-    select {|token| not WORDS_TO_IGNORE.include? token.downcase} # filter common
 end
