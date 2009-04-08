b8afe75db958bdb4aa28aee152e9bdea28aa85fe
Sergio Fierens <sergio.fierens@gmail.com>
2009-04-08 12:48:16 +0100

    Main require file updated with new algorithms

--- lib/ai4r.rb
+++ lib/ai4r.rb
@@ -1,6 +1,7 @@
 # Data
 require "ai4r/data/data_set"
 require "ai4r/data/statistics"
+require "ai4r/data/proximity"
 require "ai4r/data/parameterizable"
 # Clusterers
 require "ai4r/clusterers/clusterer"
@@ -9,12 +10,18 @@ require "ai4r/clusterers/bisecting_k_means"
 require "ai4r/clusterers/single_linkage"
 require "ai4r/clusterers/complete_linkage"
 require "ai4r/clusterers/average_linkage"
+require "ai4r/clusterers/weighted_average_linkage"
+require "ai4r/clusterers/centroid_linkage"
+require "ai4r/clusterers/median_linkage"
+require "ai4r/clusterers/ward_linkage"
+require "ai4r/clusterers/diana"
 # Classifiers
 require "ai4r/classifiers/classifier"
 require "ai4r/classifiers/id3"
 require "ai4r/classifiers/prism"
 require "ai4r/classifiers/one_r"
 require "ai4r/classifiers/zero_r"
+require "ai4r/classifiers/hyperpipes"
 # Neural networks
 require "ai4r/neural_network/backpropagation"
 # Genetic Algorithms
