0a7582886f2befc9228bf16d8a239a9404af8e01
Thomas Kern <thomaskern@thomas-kerns-mac-pro.local>
2009-06-12 21:18:55 +0200

    removed multi-calculation from som example

--- examples/som/som_example.rb
+++ examples/som/som_example.rb
@@ -2,28 +2,23 @@ require File.dirname(__FILE__) + '/../../lib/ai4r/som/som'
 require File.dirname(__FILE__) + '/som_data'
 require 'benchmark'
 
-10.times do |t|
-  t += 3
+som = Ai4r::Som::Som.new 4, 8, Ai4r::Som::TwoPhaseLayer.new(7, 0.95, 200,150)
+som.initiate_map
 
-  puts "Nodes: #{t}"
-  som = Ai4r::Som::Som.new 4, 8, Ai4r::Som::Layer.new(t, t - 1, 200, 0.95)
-  som.initiate_map
-
-  som.nodes.each do |node|
-    #p node.weights
-  end
-
-  puts "global error distance: #{som.global_error(SOM_DATA)}"
-  puts "\ntraining the som\n"
-
-  times = Benchmark.measure do
-    som.train SOM_DATA
-  end
-
-  som.nodes.each do |node|
-    p node.weights
-  end
-
-  puts "Elapsed time for training: #{times}"
-  puts "global error distance: #{som.global_error(SOM_DATA)}\n\n"
+som.nodes.each do |node|
+  p node.weights
 end
+
+puts "global error distance: #{som.global_error(SOM_DATA)}"
+puts "\ntraining the som\n"
+
+times = Benchmark.measure do
+  som.train SOM_DATA
+end
+
+som.nodes.each do |node|
+  p node.weights
+end
+
+puts "Elapsed time for training: #{times}"
+puts "global error distance: #{som.global_error(SOM_DATA)}"
