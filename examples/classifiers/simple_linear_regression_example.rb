4654ff0c35185bd76433a6e0f8e6ebfebf949d38
Sergio Fierens <sergio.fierens@gmail.com>
2013-03-11 13:51:38 -0500

    Revert "Merge pull request #11 from malavbhavsar/master" sue to issues with rarff requirements
    This reverts commit 9bf22cdec21f9dd7e3897fda51f509e747db7370, reversing
    changes made to 794d03ac46a89fa4d2061d635bebf0d0d43b8941.

--- examples/classifiers/simple_linear_regression_example.rb
+++ examples/classifiers/simple_linear_regression_example.rb
@@ -13,9 +13,3 @@ p r.eval([-1,95,109.1,188.8,68.9,55.5,3062,141,3.78,3.15,9.5,114,5400,19,25])
 
 # => 11662.949367088606
 #Actual price 22625
-
-data_set = DataSet.new
-data_set.parse_arff_with_labels File.dirname(__FILE__) + "/simple_linear_regression_example.arff"
-
-r = SimpleLinearRegression.new.build data_set
-p r.eval([-1,95,109.1,188.8,68.9,55.5,3062,141,3.78,3.15,9.5,114,5400,19,25])
