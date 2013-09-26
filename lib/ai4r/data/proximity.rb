34be703c48f61675d77595849e08c981c9ca9f7e
Vladimir Ivic <vlada.ivic@gmail.com>
2013-09-25 17:39:41 -0700

    Adding a method for calculating cosine distance

--- lib/ai4r/data/proximity.rb
+++ lib/ai4r/data/proximity.rb
@@ -92,6 +92,26 @@ module Ai4r
         return 1.0/similarity - 1
       end      
       
+      # Cosine similarity is a measure of similarity between two vectors 
+      # of an inner product space that measures the cosine of the 
+      # angle between them (http://en.wikipedia.org/wiki/Cosine_similarity).
+      # 
+      # Parameters a and b are vectors with continuous attributes.
+      #
+      # D = sum(a[i] * b[i]) / sqrt(sum(a[i]**2)) * sqrt(sum(b[i]**2))
+      def self.cosine_distance(a,b)
+        dot    = 0.0
+        norm_a = 0.0
+        norm_b = 0.0
+        
+        a.each_index do |i|
+          dot    += a[i] * b[i]
+          norm_a += a[i] ** 2
+          norm_b += b[i] ** 2
+        end
+        
+        dot / (Math.sqrt(norm_a) * Math.sqrt(norm_b))
+      end
     end
     
   end
